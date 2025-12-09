acffabf652cf50a19e43863f6b2b6663546a82e3
Madina Yerzhimbay <250523@astanait.edu.kz>
2025-12-09 21:19:00 +0500

    Add source code

--- src/com/madina/vetclinic/Main.java
+++ src/com/madina/vetclinic/Main.java
@@ -3,17 +3,21 @@ package com.madina.vetclinic;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("=== Welcome to the Veterinary Clinic Management System ===");
-        System.out.println("This project focuses on managing veterinary clinic operations efficiently.\n");
-        System.out.println("Our system helps automate pet registration, appointment scheduling, and medical record tracking.\n");
-        System.out.println("\nMain features we plan to implement:");
-        System.out.println("1. Pet registration and owner information management");
-        System.out.println("2. Veterinarian appointment scheduling");
-        System.out.println("3. Medical history and treatment record tracking");
-        System.out.println("4. Billing and payment processing");
-        System.out.println("5. Inventory management for medicines and supplies");
+        System.out.println("""
+        === Welcome to the Veterinary Clinic Management System ===
+        This project focuses on managing veterinary clinic operations efficiently.
+        Our system helps automate pet registration, appointment scheduling, and medical record tracking.
 
-        System.out.println("\nDeveloped by: Madina Yerzhimbay");
-        System.out.println("Course: Object-Oriented Programming (OOP)");
+        Main features we plan to implement:
+        1. Pet registration and owner information management
+        2. Veterinarian appointment scheduling
+        3. Medical history and treatment record tracking
+        4. Billing and payment processing
+        5. Inventory management for medicines and supplies
+
+        Developed by: Madina Yerzhimbay
+        Course: Object-Oriented Programming (OOP)
+        """);
+
     }
 }
